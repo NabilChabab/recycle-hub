df97f27ed975c8b831e3ad5b074c44b709f15b20
NabilChabab <nabil.chababnabil@gmail.com>
2025-02-05 11:30:48 +0100

    user auth

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'recycle-hub' title`, () => {
+  it(`should have the 'RecycleHub' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('recycle-hub');
+    expect(app.title).toEqual('RecycleHub');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, recycle-hub');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, RecycleHub');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'recycle-hub';
+  title = 'RecycleHub';
 }
